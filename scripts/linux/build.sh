e81c89efb665d23419051ac7c064ec0ae80ae5f1
kabies <37025477+kabies@users.noreply.github.com>
2018-03-16 17:56:41 +0900

    build in linux

---
+++ scripts/linux/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+REPOSITORY="https://github.com/mruby/mruby.git"
+DIR="build/linux"
+MRUBY_CONFIG="$PWD/source/build_config-1.4.0-linux.rb"
+
+echo "MRUBY_CONFIG=$MRUBY_CONFIG"
+
+mkdir -p "build"
+git clone -b 1.4.0 $REPOSITORY $DIR
+
+cd $DIR
+env FRAMEWORKS_PATH=$FRAMEWORKS_PATH MRUBY_CONFIG=$MRUBY_CONFIG ruby minirake -v all
+
+cd -
+cp build/linux/build/host/bin/mruby bin/mruby
+cp build/linux/build/host/bin/mirb bin/mirb
+cp build/linux/build/host/bin/mrbc bin/mrbc
---
+++ source/build_config-1.4.0-linux.rb
@@ -0,0 +1,37 @@
+
+MRuby::Build.new do |conf|
+  toolchain :gcc
+
+  conf.gem github: 'mruby-sdl2/mruby-sdl2'
+  conf.gem github: 'mruby-sdl2/mruby-sdl2-mixer'
+  conf.gem github: 'mruby-sdl2/mruby-sdl2-ttf'
+  conf.gem github: 'mruby-sdl2/mruby-sdl2-image'
+  conf.gem github: 'mruby-sdl2/mruby-sdl2-partial-rwops'
+  conf.gem github: 'mruby-sdl2/mruby-sdl2-scene-graph'
+
+  conf.gem github: 'kabies/mruby-stable-sort'
+
+  # conf.gem :github => 'AndrewBelt/mruby-yaml' # incompatible for crosscompile
+  # conf.gem :mgem => 'mruby-yaml' # incompatible 1.4.0
+  conf.gem :github => 'mruby-Forum/mruby-yaml', :branch => 'fix-build'
+
+  conf.gem :mgem => 'mruby-singleton'
+  conf.gem :github => 'matsumoto-r/mruby-sleep'
+  conf.gem :github => 'iij/mruby-env'
+  conf.gem :github => 'iij/mruby-iijson'
+  conf.gem :github => 'iij/mruby-dir'
+  conf.gem :core => 'mruby-io'
+  conf.gem :core => 'mruby-pack'
+  conf.gem :core => 'mruby-socket'
+  conf.gem :core => 'mruby-exit'
+  conf.gembox 'default'
+
+  #
+  # compiler settings
+  #
+
+  conf.cc do |cc|
+    cc.defines += %w(MRB_UTF8_STRING MRB_32BIT)
+  end
+
+end
