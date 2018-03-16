7114478520a358d265cbe92b511a93c7276f0176
kabies <37025477+kabies@users.noreply.github.com>
2018-03-16 16:04:48 +0900

    scripts/macos/build.sh, build-mingw-w64.sh: add new build script

---
+++ scripts/macos/build-mingw-w64.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+REPOSITORY="https://github.com/mruby/mruby.git"
+DIR="build/mingw-w64"
+MRUBY_CONFIG="$PWD/source/build_config-1.4.0-w64.rb"
+CROSS_TOOLS_PATH="/usr/local"
+
+echo "MRUBY_CONFIG=$MRUBY_CONFIG"
+
+mkdir -p "build"
+git clone -b 1.4.0 $REPOSITORY $DIR
+
+cd $DIR
+env CROSS_TOOLS_PATH=$CROSS_TOOLS_PATH MRUBY_CONFIG=$MRUBY_CONFIG ruby minirake -v all
---
+++ scripts/macos/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+REPOSITORY="https://github.com/mruby/mruby.git"
+DIR="build/macos"
+MRUBY_CONFIG="$PWD/source/build_config-1.4.0-macos.rb"
+FRAMEWORKS_PATH="$HOME/Library/Frameworks"
+
+echo "MRUBY_CONFIG=$MRUBY_CONFIG"
+
+mkdir -p "build"
+git clone -b 1.4.0 $REPOSITORY $DIR
+
+cd $DIR
+env FRAMEWORKS_PATH=$FRAMEWORKS_PATH MRUBY_CONFIG=$MRUBY_CONFIG ruby minirake -v all
+
+cd -
+cp build/macos/build/host/bin/mruby bin/mruby
+cp build/macos/build/host/bin/mirb bin/mirb
+cp build/macos/build/host/bin/mrbc bin/mrbc
