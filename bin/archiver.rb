788f98d2e87a67019ccdd8cc8ef7c1ba9ff22332
kabies <37025477+kabies@users.noreply.github.com>
2018-04-02 17:13:13 +0900

    archiver: to archive recursive. 4CC changed to BIAR. filename includes top level directory.

--- bin/archiver.rb
+++ bin/archiver.rb
@@ -1,37 +1,53 @@
+#!/usr/bin/env mruby
 
-FourCC = "CNBR"
+FourCC = "BIAR" # bismuth archive
 VERSION = 1
 
 dir = ARGV[0]
-target_dir = File.join dir, "assets"
+unless dir
+  dir = "assets"
+end
 
-file_list = []
-Dir.foreach(target_dir){|f|
-  next if f[0] == "."
-  file = File.join target_dir, f
-  if File.exist?(file) and File.file?(file)
-    file_list << [f,File::size(file)]
+unless File.exists?(dir) and File.directory?(dir)
+  STDERR.puts "#{dir} is not directory"
+  exit 1
+end
+
+def files(dir)
+  file_list = []
+  if File.exists?(dir) and File.directory?(dir)
+    Dir.foreach(dir){|f|
+      # skip hidden files and current/parent directory
+      next if f[0] == "."
+      file = File.join dir, f
+      if File.exist?(file)
+        if File.file?(file)
+          file_list << [file,File::size(file)]
+        elsif File.directory?(file)
+          file_list += files(file)
+        end
+      end
+    }
   end
-}
+  file_list
+end
 
-archive = File.open "archive", "w"
+file_list = files(dir)
 
-archive.write FourCC
-archive.write [VERSION].pack "V"
-archive.write [file_list.size].pack "V"
-file_start = 0
-file_list.each{|f,size|
-  filename_length = f.size
-  p [size, file_start, filename_length, f]
-  archive.write [size, file_start, filename_length].pack "VVV"
-  archive.write f
-  file_start += size
-}
+File.open("assets.biar", "w") do |archive|
+  archive.write FourCC
+  archive.write [VERSION].pack "V"
+  archive.write [file_list.size].pack "V"
+  file_start = 0
+  file_list.each{|filename,size|
+    p [size, file_start, filename.length, filename]
+    archive.write [size, file_start, filename.length].pack "VVV"
+    archive.write filename
+    file_start += size
+  }
 
-file_list.each{|f,size|
-  file = File.join target_dir, f
-  dat = File.read file
-  archive.write dat
-}
-
-archive.close
+  file_list.each{|f,size|
+    dat = File.read(f)
+    archive.write dat
+  }
+end
